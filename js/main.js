9f8014b0ba7d08d54c8f0b93c55042705237fa62
Nicolo <nicolorizzi1996@gmail.com>
2020-04-03 16:51:37 +0200

    BLOCCO 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-/**
+/******************************************************************
  * ESERCIZIO 1 DEL JSNACK
  * Snack 1: L’utente inserisce due parole in successione, con due prompt. Il software stampa la parola più lunga. Utilizza una funzione per determinare e ritornare la parola più lunga.
- */
+ *****************************************************************/
+/*
 var parola1 = prompt('Inserisci la prima parola');
 var parola2 = prompt('Inserisci la seconda parola');
 var confronta = stringaMaggiore(parola1, parola2);
-console.log(confronta);
+console.log('La stringa con più caratteri è',confronta);
 
 
 function stringaMaggiore(par1, par2) {
@@ -23,4 +24,23 @@ function stringaMaggiore(par1, par2) {
     
   }
   return res;
+}
+*/
+/*********************************************************************
+ * Snack 2: 
+ * Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+ *********************************************************************/
+var numero = parseInt(prompt('Inserisci un numero'));
+var res = pippo(numero);
+console.log(res);
+
+// VOGLIO PROVARE IL BRIVIDO DI CHIAMARE LA FUNZIONE PIPPO PERDONATEMI :(
+function pippo(par1) {
+  if (par1 % 2 == 0) {
+
+  } else if (par1 % 2 !== 0) {
+    ++par1;
+
+  }
+  return par1;
 }
