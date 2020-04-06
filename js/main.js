d7c76a1f8ae9eacdca094083db5a921869638d64
Nicolo <nicolorizzi1996@gmail.com>
2020-04-06 09:33:41 +0200

    FIXING

--- js/main.js
+++ js/main.js
@@ -7,11 +7,6 @@ var parola1 = prompt('Inserisci la prima parola');
 var parola2 = prompt('Inserisci la seconda parola');
 var confronta = stringaMaggiore(parola1, parola2);
 console.log('La stringa con più caratteri è',confronta);
-if (confronta === false) {
-
-} else {
-
-}
 
 function stringaMaggiore(par1, par2) {
   var res = "";
@@ -34,7 +29,7 @@ function stringaMaggiore(par1, par2) {
  * Snack 2: 
  * Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
  *********************************************************************/
-/*
+
 var numero = parseInt(prompt('Inserisci un numero'));
 var res = pippo(numero);
 console.log(res);
@@ -42,15 +37,17 @@ console.log(res);
 // VOGLIO PROVARE IL BRIVIDO DI CHIAMARE LA FUNZIONE PIPPO PERDONATEMI :(
 function pippo(par1) {
   if (par1 % 2 == 0) {
-
+    console.log('Il tuo numero indicato rimane invariato');
+    
   } else if (par1 % 2 !== 0) {
     ++par1;
+    console.log('Il tuo numero',numero,'è stato incrementato di 1');
+    
 
   }
   return par1;
 }
-*/
-/*******************************************************************************
+/*********************************************************************************
  * Snack 3: Generatore di “nomi cognomi” casuali: prendendo una lista di nomi (‘Michele’, ‘Fabio’, ‘Roberto’) e una lista di cognomi (‘Forghieri’, ‘Papagni’, ‘Marazzini’), Gatsby vuole generare una falsa lista di 5 invitati.
  ******************************************************************************/
 /*
